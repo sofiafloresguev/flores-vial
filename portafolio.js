92924b2be34e722b9c46d594222c85e3f205fda2
sofiafloresguev <54024506+sofiafloresguev@users.noreply.github.com>
2019-08-19 08:38:10 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -14,22 +14,22 @@ var trabajos = [{
     title: "Título de tercer trabajo",
     about: "Texto simulado de descripción de tercer trabajo"
 }, {
-    figure: "media/trabajo-04-editorial.jpg",
+    figure: "media/borrador.png",
     alt: "Descripción de la imagen",
     title: "Título de cuarto trabajo",
     about: "Texto simulado de descripción de cuarto trabajo"
 }, {
-    figure: "media/trabajo-05-artesania.jpg",
+    figure: "media/zoom.png",
     alt: "Descripción de la imagen",
     title: "Título de quinto trabajo",
     about: "Texto simulado de descripción de quinto trabajo"
 }, {
-    figure: "media/trabajo-06-textil.jpg",
+    figure: "media/mural.png",
     alt: "Descripción de la imagen",
     title: "Título de sexto trabajo",
     about: "Texto simulado de descripción de sexto trabajo"
 }, {
-    figure: "media/trabajo-07-materiales.jpg",
+    figure: "media/mako.png",
     alt: "Descripción de la imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo"
